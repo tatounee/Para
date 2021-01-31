1c484ea43ccbd8e2a941479b7eb42bd7a4c118aa
T4toun3 <oettam32@gmail.com>
2021-01-31 18:36:09 +0100

    Create Clap Appication with env var

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,36 @@ use std::fs::File;
 use std::process::exit;
 use std::env;
 fn main() {
+    dotenv().ok();
+
+    let matches =  App::new("Paranagram")
+        .author(crate_authors!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .bin_name("paranagram")
+        .arg(Arg::with_name("SENTENCE")
+            .help("The sentence from where you want to find anagrams")
+            .required(true))
+        .arg(Arg::with_name("INPUT")
+            .help("Set the input file which be use as a dictionary")
+            .short("i")
+            .long("input")
+            .env("INPUT_PATH")
+            .default_value("words.txt")
+            .takes_value(true))
+        .arg(Arg::with_name("OUTPUT")
+            .help("Set the output file")
+            .short("o")
+            .long("output")
+            .env("OUTPUT_PATH")
+            .takes_value(true))
+        .arg(Arg::with_name("JUSTWORDS")
+            .help("Use if you only want the word witch can be create from your sentence")
+            .short("j")
+            .long("justwords"))
+        .arg(Arg::with_name("DEBUG")
+            .help("Prints advencement of anagrams's generation")
+            .short("d")
+            .long("debug")
+            .possible_values(&["true", "false"]))
+        .get_matches();
