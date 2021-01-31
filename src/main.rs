fb0f169471ab1a4375d36df6202945f3cdf0f58a
T4toun3 <oettam32@gmail.com>
2021-01-31 18:35:20 +0100

    Import dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
+
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg};
+use dotenv::dotenv;
+use paranagram::{Paranagram, Word};
+
+use std::path::Path;
+use std::io::Write;
+use std::fs::File;
+use std::process::exit;
+use std::env;
 fn main() {
